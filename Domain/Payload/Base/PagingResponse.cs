9204032eece64146eb7e8cfe7a3844950e4c2a0e
baodev <tqbao.work@gmail.com>
2025-06-24 17:44:27 +0700

    Config Paging

---
+++ Domain/Payload/Base/PagingResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Payload.Base
+{
+    public class PagingResponse<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+        public List<T> Items { get; set; }
+
+        public PagingResponse(List<T> items, int pageNumber, int pageSize, int totalCount)
+        {
+            Items = items;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+        }
+    }
+
+}
